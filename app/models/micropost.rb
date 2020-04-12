34853ee5e35ba150ba3f300fe06105a82f562724
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-12 11:47:12 +0300

    add validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ApplicationRecord
-    validates :context, length: { maximum: 140 }
+    belongs_to :user
+    validates :context, length: { maximum: 140 }, presence: true
+    validates :user_id, presence: true
 end
